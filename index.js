114b3a2936aab81a77709cc2da956ac6e8e5d943
saimundev <saimunshezan@gmail.com>
2024-01-13 11:29:12 +0600

    online-user and message

---
+++ index.js
@@ -0,0 +1,44 @@
+import { Server } from "socket.io";
+
+const io = new Server({
+  cors: "http://localhost:3000/",
+});
+
+const port = process.env.PORT || 5000;
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+io.on("connection", (socket) => {
+  //add user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUser", users);
+  });
+
+  //send and get message
+  socket.on("sendMessage", (message) => {
+    const user = users.find((user) => user.userId === message.receiverId);
+    if (user) {
+      io.to(user.socketId).emit("getMessage", message);
+    }
+  });
+
+  //disconnect user
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+    io.emit("getUser", users);
+  });
+});
+
+io.listen(port, () => {
+  console.log("socket server runing");
+});
